9e666480b42bc7b21e2c4a8dc7d2b6974221740e
Randolf Herrera <randolf_08@hotmail.com>
2024-12-07 19:20:57 -0500

    fix shiny

--- Presentacion/shiny/dashboar/shared.py
+++ Presentacion/shiny/dashboar/shared.py
@@ -1,6 +1,9 @@
 from pathlib import Path
-
+import geopandas as gpd
 import pandas as pd
 
 app_dir = Path(__file__).parent
-df = pd.read_csv(app_dir / "penguins.csv")
+#Data Frame
+df = pd.read_csv(app_dir / "../../../Data/db/Procesados/data_limpia.csv")
+#Shape File
+shp = gpd.read_file(app_dir / "../../../Data/shapefiles/Barrios/Barrios.shp")
